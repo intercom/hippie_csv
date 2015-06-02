7f85d76b1db31328cb8b9f092a30685bf70d4868
Stephen O'Brien <stephen@intercom.io>
2015-06-01 23:24:49 -0700

    Initial extraction & de-railsing of code

--- hippie_csv.gemspec
+++ hippie_csv.gemspec
@@ -8,8 +8,7 @@ Gem::Specification.new do |spec|
   spec.version       = HippieCsv::VERSION
   spec.authors       = ["Stephen O'Brien"]
   spec.email         = ["stephen@intercom.io"]
-  spec.summary       = %q{TODO: Write a short summary. Required.}
-  spec.description   = %q{TODO: Write a longer description. Optional.}
+  spec.summary       = %q{Liberally parse CSV files}
   spec.homepage      = ""
   spec.license       = "MIT"
 
@@ -20,4 +19,5 @@ Gem::Specification.new do |spec|
 
   spec.add_development_dependency "bundler", "~> 1.5"
   spec.add_development_dependency "rake"
+  spec.add_development_dependency "rspec"
 end
---
+++ spec/hippie_csv_spec.rb
@@ -0,0 +1,66 @@
+require "spec_helper"
+require "CSV"
+
+describe HippieCsv do
+
+  def fixture_path(name)
+    File.expand_path("../fixtures/#{name}.CSV", __FILE__)
+  end
+
+  context 'hard/encountered problem cases' do
+    it 'works when a BOM is present in the file' do
+      path = fixture_path(:with_byte_order_mark)
+      expect { CSV.read(path) }.to raise_error(CSV::MalformedCSVError)
+
+      import = HippieCsv.parse(path)
+      expect(import[0]).to eq(['Name', 'Email Address', 'Date Added'])
+    end
+
+    it 'works with a malformed CSV' do
+      path = fixture_path(:malformed)
+      expect { CSV.read(path) }.to raise_error(CSV::MalformedCSVError)
+
+      import = HippieCsv.parse(path)
+      expect(import[0]).to eq(%w(site lon lat max min precip snow snowdepth))
+    end
+
+    it 'works with odd encoding & emoji!' do
+      path = fixture_path(:encoding)
+      expect { CSV.read(path) }.to raise_error(ArgumentError)
+
+      import = HippieCsv.parse(path)
+      expect(import[0].count).to eq(4)
+    end
+
+    it 'works with an excel export' do
+      path = fixture_path(:excel)
+
+      import = HippieCsv.parse(path)
+      expect(import[0].count).to eq(24)
+    end
+
+    it 'works with escaped quotes' do
+      path = fixture_path(:escaped_quotes)
+
+      import = HippieCsv.parse(path)
+      expect(import[0][1]).to eq("Lalo \"ElPapi\" Neymar")
+      expect(import[0][2]).to eq("lalo@example.com")
+    end
+
+    it 'works with an invalid escaped quotes case' do
+      path = fixture_path(:escaped_quotes_semicolons)
+
+      import = HippieCsv.parse(path)
+      expect(import[0][0]).to eq("133")
+      expect(import[0][1]).to eq("z3268856")
+      expect(import[0][2]).to eq("stephen@example.com")
+    end
+
+    it 'works for a complicated case involving bad newlines and quote chars' do
+      path = fixture_path(:dos_line_ending)
+
+      import = HippieCsv.parse(path)
+      expect(import[0].count).to eq(9)
+    end
+  end
+end
