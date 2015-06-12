c8efcb53e786afd54929c2b6f14d00711f3da0fa
Stephen O'Brien <stephen@intercom.io>
2015-06-12 13:53:48 +0100

    Uppercase CSV

--- lib/hippie_csv/errors.rb
+++ lib/hippie_csv/errors.rb
@@ -1,4 +1,4 @@
-module HippieCsv
+module HippieCSV
   class UnableToParseError < StandardError
     def self.explain
       raise new "Something went wrong. Report this CSV: https://github.com/intercom/hippie_csv"
