df41d8b140ef02c6bad3f07f872d19ecbf8f890f
Stephen O'Brien <stephen@intercom.io>
2015-09-10 15:34:25 -0700

    Bump version

--- spec/hippie_csv/version_spec.rb
+++ spec/hippie_csv/version_spec.rb
@@ -3,7 +3,7 @@ require "spec_helper"
 describe HippieCSV do
 
   it "defines a version" do
-    expect(HippieCSV::VERSION).to eq("0.0.7")
+    expect(HippieCSV::VERSION).to eq("0.0.8")
   end
 
 end
