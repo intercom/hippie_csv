df9091b6b5edb12de4bbacd4e397417044682e35
Jessica O'Brien <j.e.obrien28@gmail.com>
2017-08-08 18:14:47 -0700

    Bumping version to 0.1.0

--- spec/hippie_csv/version_spec.rb
+++ spec/hippie_csv/version_spec.rb
@@ -3,7 +3,7 @@ require "spec_helper"
 describe HippieCSV do
 
   it "defines a version" do
-    expect(HippieCSV::VERSION).to eq("0.0.11")
+    expect(HippieCSV::VERSION).to eq("0.1.0")
   end
 
 end
